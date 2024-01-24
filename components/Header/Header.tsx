0ccfb74af7c9e812cd701099d8a7504cd161233d
ByteBigBoss <nethminas42@gmail.com>
2024-01-24 19:05:28 +0530

    changes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,19 @@
 import Image from 'next/image'
 import React from 'react'
 
+import logo from '@/public/logo.png';
 
 const Header = () => {
   return (
     <div>
       <nav>
-        <div className="logo"></div>
-        <Image src={logo} alt='logo' width={128} height={77}/>
+        <div className="logo">
+          <Image src={logo} alt='logo' width={36} />
+          <h2>Logo</h2>
+        </div>
+        <ul className="nav-items">
+          
+        </ul>
       </nav>
     </div>
   )
