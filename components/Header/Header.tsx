4bf396c5099992fb92698dbe9b305d9591fa78b2
ByteBigBoss <nethminas42@gmail.com>
2024-01-24 22:43:37 +0530

    changes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,21 +3,81 @@ import Image from 'next/image'
 import React from 'react'
 
 import logo from '@/public/logo.png';
+import Button from '../Button/Button';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <div>
+    <HeaderStyled>
       <nav>
         <div className="logo">
           <Image src={logo} alt='logo' width={36} />
           <h2>Logo</h2>
         </div>
+
+        <div className="input">
+            <input type="text" placeholder='Search' />
+        </div>
+
         <ul className="nav-items">
-          
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Auctions</a>
+          </li>
+          <li>
+            <a href="#">Marketplace</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <Button/>
         </ul>
       </nav>
-    </div>
+    </HeaderStyled>
   )
 }
+
+const HeaderStyled = styled.header`
+
+  nav{
+    padding: 0 4rem;
+    min-height: 10vh;
+    border-bottom: 1px solid var(--color-border);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo{
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      cursor: pointer;
+    }
+
+    .input{
+      flex: 2;
+      display: flex;
+      justify-content: center;
+    }
+
+    .nav-items{
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+      li{
+        transition: all 0.2s ease-in-out;
+
+        &:hover{
+          color: white;
+          transform: scale(1.1);
+        }
+      }
+    }
+
+    
+  }
+
+`;
 
 export default Header
